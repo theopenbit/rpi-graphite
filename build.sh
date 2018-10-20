5471c5023342beb1be80b4f61626c7f560d00a90
theOpenbit <tob@schoenesnetz.de>
2018-10-20 23:21:51 +0200

    use offical ubuntu base image

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ mkdir build
 cd build
 git clone -b v1.1.4-4 https://github.com/graphite-project/docker-graphite-statsd.git
 cd docker-graphite-statsd
-sed -i '1s/.*/FROM resin\/rpi-raspbian:jessie/' Dockerfile
+sed -i '1s/.*/FROM ubuntu:18.04/' Dockerfile
 DOCKER_TAG='latest'
 if [ -n "$TRAVIS_TAG" ]; then DOCKER_TAG=$TRAVIS_TAG; fi
 docker build -t theopenbit/rpi-graphite:$DOCKER_TAG .
