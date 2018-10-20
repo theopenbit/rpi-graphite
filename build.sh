b09f29ba2f1a47ef61d68c6d5b9b7cdcf9fe57ae
theOpenbit <tob@schoenesnetz.de>
2018-10-20 23:38:23 +0200

    add noninteractive

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ cd build
 git clone -b v1.1.4-4 https://github.com/graphite-project/docker-graphite-statsd.git
 cd docker-graphite-statsd
 sed -i '1s/.*/FROM ubuntu:18.04/' Dockerfile
+sed -n -i 'p;3a ENV DEBIAN_FRONTEND=noninteractive' Dockerfile
 DOCKER_TAG='latest'
 if [ -n "$TRAVIS_TAG" ]; then DOCKER_TAG=$TRAVIS_TAG; fi
 docker build -t theopenbit/rpi-graphite:$DOCKER_TAG .
