8beb67ec28f92287ab824c1c36b10e3f90ff49ec
theOpenbit <tob@schoenesnetz.de>
2018-10-21 16:21:21 +0200

    wip

--- build.sh
+++ build.sh
@@ -6,13 +6,10 @@ mkdir build
 cd build
 git clone -b v1.1.4-4 https://github.com/graphite-project/docker-graphite-statsd.git
 cd docker-graphite-statsd
-#basetag=$(docker manifest inspect ubuntu:18.04 | jq -r '.manifests[] | select(.platform.architecture == "arm" and .platform.variant == "v7") | .digest')
-#sed -i "1s/.*/FROM ubuntu@$basetag/" Dockerfile
-sed -i "1s/.*/FROM armhf/ubuntu:17.04/" Dockerfile
-sed -n -i 'p;3a MAINTAINER theOpenbit <tob@schoenesnetz.de>' Dockerfile
-sed -n -i 'p;4a ENV DEBIAN_FRONTEND=noninteractive' Dockerfile
+sed -i '1s/.*/FROM theopenbit\/rpi-ubuntu:latest/' Dockerfile
+sed -n -i 'p;3a MAINTAINER theOpenbit' Dockerfile
 DOCKER_TAG='latest'
 if [ -n "$TRAVIS_TAG" ]; then DOCKER_TAG=$TRAVIS_TAG; fi
-docker build -t theopenbit/rpi-graphite:$DOCKER_TAG .
+docker build --build-arg CONTAINER_TIMEZONE=Europe/Brussels -t theopenbit/rpi-graphite:$DOCKER_TAG .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push theopenbit/rpi-graphite:$DOCKER_TAG
