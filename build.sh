a4aa2fdbc2c70c4794542d02bf61d3b0e6647263
theOpenbit <tob@schoenesnetz.de>
2018-10-21 17:07:08 +0200

    wip

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir build
 cd build
 git clone -b v1.1.4-4 https://github.com/graphite-project/docker-graphite-statsd.git
 cd docker-graphite-statsd
-sed -i '1s/.*/FROM theopenbit\/rpi-ubuntu:latest/' Dockerfile
+sed -i "1s/.*/FROM theopenbit\/rpi-baseimage:latest/" Dockerfile
 sed -n -i 'p;3a MAINTAINER theOpenbit' Dockerfile
 DOCKER_TAG='latest'
 if [ -n "$TRAVIS_TAG" ]; then DOCKER_TAG=$TRAVIS_TAG; fi
