6148250c02602d6930f1d60e00db34f6c4edbc0a
theOpenbit <tob@schoenesnetz.de>
2018-10-21 09:29:50 +0200

    wip

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# ~/.docker/config.json must include
+#  "experimental": "enabled"
+#
 mkdir build
 cd build
 git clone -b v1.1.4-4 https://github.com/graphite-project/docker-graphite-statsd.git
 cd docker-graphite-statsd
-sed -i '1s/.*/FROM ubuntu:18.04/' Dockerfile
-sed -n -i 'p;3a ENV DEBIAN_FRONTEND=noninteractive' Dockerfile
+basetag=$(docker manifest inspect ubuntu:18.04 | jq -r '.manifests[] | select(.platform.architecture == "arm" and .platform.variant == "v7") | .digest')
+sed -i "1s/.*/FROM ubuntu@$basetag/" Dockerfile
+sed -n -i 'p;3a MAINTAINER theOpenbit <tob@schoenesnetz.de>' Dockerfile
+sed -n -i 'p;4a ENV DEBIAN_FRONTEND=noninteractive' Dockerfile
 DOCKER_TAG='latest'
 if [ -n "$TRAVIS_TAG" ]; then DOCKER_TAG=$TRAVIS_TAG; fi
 docker build -t theopenbit/rpi-graphite:$DOCKER_TAG .
